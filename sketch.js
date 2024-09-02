adc6545174134ecbab288c28f684f19b532ebd4b
jkzt1 <jk.zt111@gmail.com>
2024-09-02 20:59:41 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+}
+
+function draw() {
+  stoke("blue");
+  fill("red");
+  
+  1f(mouseiPressed){
+    react(mouseX, mouseY, 20,35);
+  }
+  
+}
